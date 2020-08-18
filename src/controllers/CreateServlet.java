3c4d709748b568fc13a18e760b88159450384379
natsume4948 <kazu.100817@gmail.com>
2020-08-18 18:52:45 +0900

    Add validation

--- src/controllers/CreateServlet.java
+++ src/controllers/CreateServlet.java
@@ -2,8 +2,10 @@ package controllers;
 
 import java.io.IOException;
 import java.sql.Timestamp;
+import java.util.List;
 
 import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -12,6 +14,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import models.Tasklist;
 import utils.DBUtil;
+import validators.TasklistValidator;
 /**
  * Servlet implementation class CreateServlet
  */
@@ -44,12 +47,30 @@ public class CreateServlet extends HttpServlet {
             m.setCreated_at(currentTime);
             m.setUpdated_at(currentTime);
 
-            em.getTransaction().begin();
-            em.persist(m);
-            em.getTransaction().commit();
-            em.close();
+            List<String> errors = TasklistValidator.validate(m);
+            if(errors.size() > 0) {
+                em.close();
 
-            response.sendRedirect(request.getContextPath() + "/index");
+
+                request.setAttribute("_token", request.getSession().getId());
+                request.setAttribute("tasklist", m);
+                request.setAttribute("errors", errors);
+
+                RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/tasks/new.jsp");
+                rd.forward(request, response);
+            } else {
+
+                em.getTransaction().begin();
+                em.persist(m);
+                em.getTransaction().commit();
+                request.getSession().setAttribute("flush", "登録が完了しました。");
+                em.close();
+
+
+                response.sendRedirect(request.getContextPath() + "/index");
+            }
+
+
         }
     }
 
--- src/controllers/IndexServlet.java
+++ src/controllers/IndexServlet.java
@@ -35,14 +35,31 @@ public class IndexServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         EntityManager em = DBUtil.createEntityManager();
 
+        int page = 1;
+        try {
+            page = Integer.parseInt(request.getParameter("page"));
+        } catch(NumberFormatException e) {}
+
+
         List<Tasklist> tasks = em.createNamedQuery("getAllTasks", Tasklist.class)
+                                   .setFirstResult(15 * (page - 1))
+                                   .setMaxResults(15)
                                    .getResultList();
-        response.getWriter().append(Integer.valueOf(tasks.size()).toString());
+
+
+        long tasks_count = (long)em.createNamedQuery("getTasksCount", Long.class)
+                                      .getSingleResult();
 
         em.close();
 
         request.setAttribute("tasks", tasks);
+        request.setAttribute("tasks_count", tasks_count);     // 全件数
+        request.setAttribute("page", page);
 
+        if(request.getSession().getAttribute("flush") != null) {
+            request.setAttribute("flush", request.getSession().getAttribute("flush"));
+            request.getSession().removeAttribute("flush");
+        }
         RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/tasks/index.jsp");
         rd.forward(request, response);
     }
