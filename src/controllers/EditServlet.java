4b41a1eb7f4ca4cf41022eb45b6e7d211fce5252
natsume4948 <kazu.100817@gmail.com>
2020-08-18 18:16:58 +0900

    Modify null control

--- src/controllers/EditServlet.java
+++ src/controllers/EditServlet.java
@@ -33,16 +33,19 @@ public class EditServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         EntityManager em = DBUtil.createEntityManager();
 
-        // 該当のIDのメッセージ1件のみをデータベースから取得
+
         Tasklist m = em.find(Tasklist.class, Integer.parseInt(request.getParameter("id")));
 
         em.close();
 
-        // メッセージ情報とセッションIDをリクエストスコープに登録
+
         request.setAttribute("tasklist", m);
         request.setAttribute("_token", request.getSession().getId());
 
-        // メッセージIDをセッションスコープに登録
+        if(m != null) {
+            request.getSession().setAttribute("tasklist_id", m.getId());
+        }
+
         request.getSession().setAttribute("tasklist_id", m.getId());
 
         RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/tasks/edit.jsp");
